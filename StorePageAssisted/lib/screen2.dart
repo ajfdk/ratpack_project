9ecbb0da88411b455bf09bdfce55f3c0ecb7b5b6
Jouwsiz <102042316+Jouwsiz@users.noreply.github.com>
2024-07-10 18:15:23 -0500

    Update screen2.dart

--- StorePageAssisted/lib/screen2.dart
+++ StorePageAssisted/lib/screen2.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:untitled1/main.dart';
 
-class Screen2 extends StatelessWidget {
+class Screen2 extends StatefulWidget{
+  @override
+  _Screen2State createState() => _Screen2State();
+}
+class _Screen2State extends State<Screen2> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,12 +18,13 @@ class Screen2 extends StatelessWidget {
             children: <Widget>[
             ElevatedButton(
             onPressed: () {
+              //var LocalPoints = MyHomePage().points;
               Navigator.push(
               context,
               MaterialPageRoute(builder: (context) => MyHomePage()),
             );
-              MyHomePage().points-=10;
-              //setState(()MyHomePage().points-=10);
+              //MyHomePage().points-=10;
+              setState(() => MyHomePage().points-=10);
            },
         child: Text(MyHomePage().points.toString()),
       ),

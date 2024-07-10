1ec216c2d73fadda22d7978a2baed1d48033a453
s0nicx2729 <157264174+s0nicx2729@users.noreply.github.com>
2024-07-10 10:16:59 -0500

    Moved Template to Flutter file

--- FlutterTest/src/Main.kt
+++ FlutterTest/src/Main.kt
@@ -1,3 +1,27 @@
 fun main() {
-    println("Hello World!")
+        var pet = BasePet(0, 0, 0, 0, "BasePet", Status.OK)
+        var clone = pet.copy()
+        clone.hunger = 100
+
+        println(pet.hunger)
+        println(clone.hunger)
+        clone.state = Status.SICK
+
+        println(pet.state)
+        println(clone.state)
 }
+
+enum class Status() {
+    GREAT,
+    HAPPY,
+    OK,
+    TIRED,
+    SICK
+}
+
+data class BasePet(var hunger: Int,
+                   var happiness: Int,
+                   var growth: Int,
+                   var age: Int,
+                   var name: String,
+                   var state : Status)

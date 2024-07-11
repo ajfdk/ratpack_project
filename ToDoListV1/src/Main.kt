37e054fe311f37f87177b5587616b7e97d139bf9
s0nicx2729 <157264174+s0nicx2729@users.noreply.github.com>
2024-07-11 14:57:40 -0500

    ToDoList - Update 1
    Made the ToDo a List of objects with parameters: task : string, GoalStatus: Goal, and timeGiven: Long (temp)
    
    Objects are added and removed in separate functions for later integration.
    
    List Objects can have three states currently: INCOMPLETE, DONE, and FAIL.
    
    No timer implemented yet.

--- ToDoListV1/src/Main.kt
+++ ToDoListV1/src/Main.kt
@@ -1,3 +1,36 @@
+import android.os.CountDownTimer
+import java.sql.Time
+
 fun main() {
     println("This is a test for the ToDoList Backend functionality")
+    var taskList = mutableListOf(
+        ListObject("Check", Goal.INCOMPLETE, 30000),
+        ListObject("Check-2", Goal.FAIL,0),
+        ListObject("Check-3", Goal.DONE, 18483))
+    var listSize = taskList.size
+    println(taskList)
+    listAdd(taskList, "Check-4", 1000000, listSize)
+    println(taskList)
+    listRemove(taskList, 2)
+    println(taskList)
 }
+
+enum class Goal() {
+    INCOMPLETE,
+    DONE,
+    FAIL
+}
+
+fun listAdd(taskList: MutableList<ListObject>, taskName: String, taskTime: Long, listSize: Int) {
+    taskList.add(listSize, ListObject(taskName, Goal.INCOMPLETE, taskTime))
+}
+
+fun listRemove(taskList: MutableList<ListObject>, removePos: Int) {
+    taskList.removeAt(removePos)
+}
+
+data class ListObject(var task : String,
+                      var taskStatus : Goal,
+                      var timeGiven: Long)
+
+

b1fb8124f76957fcfa9c4a2d5b05d33253548ba8
BFavor <128089988+BFavor@users.noreply.github.com>
2024-07-24 10:34:12 -0500

    Update item_model.dart
    added subclasses for food, toy, and shop items

--- lib/item_model.dart
+++ lib/item_model.dart
@@ -1,13 +1,56 @@
 class Item {
   final String name;
-  final int healthPoints;
-  final double price;
-  final String imagePath; 
+  final String imagePath;
+  final String description;
 
   Item({
     required this.name,
-    required this.healthPoints,
-    required this.price,
     required this.imagePath,
+    required this.description,
   });
+}
+
+class FoodItem extends Item {
+  final int healthPoints;
+
+  FoodItem({
+    required String name,
+    required String imagePath,
+    required String description,
+    required this.healthPoints,
+  }) : super(
+    name: name,
+    imagePath: imagePath,
+    description: description,
+  );
+}
+
+class ToyItem extends Item {
+  final int durability;
+
+  ToyItem({
+    required String name,
+    required String imagePath,
+    required String description,
+    required this.durability,
+  }) : super(
+    name: name,
+    imagePath: imagePath,
+    description: description,
+  );
+}
+
+class ShopItem extends Item {
+  final int price;
+
+  ShopItem({
+    required String name,
+    required String imagePath,
+    required String description,
+    required this.price,
+  }) : super(
+    name: name,
+    imagePath: imagePath,
+    description: description,
+  );
 }

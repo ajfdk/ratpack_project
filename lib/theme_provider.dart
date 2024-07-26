0f0d3b14ecb82b6e360316cebf76f6e528dbc6b2
BFavor <128089988+BFavor@users.noreply.github.com>
2024-07-26 00:06:46 -0500

    widget to change to and from dark mode

---
+++ lib/theme_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  ThemeData _themeData;
+
+  ThemeProvider(this._themeData);
+
+  ThemeData get themeData => _themeData;
+
+  void toggleTheme() {
+    if (_themeData == ThemeData.light()) {
+      _themeData = ThemeData.dark();
+    } else {
+      _themeData = ThemeData.light();
+    }
+    notifyListeners();
+  }
+}

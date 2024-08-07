7e26a655e59ba1b7685617745dbe2fd007919c52
s0nicx2729 <157264174+s0nicx2729@users.noreply.github.com>
2024-08-07 00:18:41 -0500

    Pet Object initial commit
    Posted a very basic and simple version of the pet object. It should be able to be used by other functions and scripts.

---
+++ lib/pet_object.dart
@@ -0,0 +1,17 @@
+class Pet {
+  final String name;
+  final int growth;
+  final int hunger;
+  final int age;
+  final int happiness;
+
+  Pet({
+    required this.name,
+    required this.growth,
+    required this.hunger,
+    required this.age,
+    required this.happiness
+  });
+}
+
+enum STATUS {great, happy, ok, tired, sick, hungry}

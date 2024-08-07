6c3ea1a1d073251479277e00bb5645b1a947a135
BFavor <128089988+BFavor@users.noreply.github.com>
2024-08-07 02:05:32 -0500

    boot from this screen

--- lib/intermediate_screen.dart
+++ lib/intermediate_screen.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:tamagotchi/screen2.dart';
+import 'package:tamagotchi/settings_screen.dart';
+import 'package:tamagotchi/main.dart';
 import 'screen1.dart';
 import 'screen_a.dart';
 import 'screen_b.dart';
 import 'screen_c.dart';
+import 'timer.dart';
+import 'package:provider/provider.dart';
+import 'theme_provider.dart';
+import 'pin_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
 
 class IntermediateScreen extends StatelessWidget {
+  int points = 20000;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,50 +25,102 @@ class IntermediateScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                icon: Image.asset('assets/todo.png'),
+                iconSize: 10,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Screen2()),
+                  );
+                }
+              ),
+              IconButton(
+                icon: Image.asset('assets/timer.png'),
+                iconSize: 10,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => TimerScreen()),
+                  );
+                },
+              ),
+              IconButton(
+                icon: Image.asset('assets/settings.png'),
+                iconSize: 10,
+                onPressed: () {
+                  bool pinProtectionEnabled = Provider.of<ThemeProvider>(context, listen: false).pinProtectionEnabled;
+                  if (pinProtectionEnabled) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => PinScreen()),
+                    );
+                  } else {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SettingsScreen()),
+                    );
+                  }
+                },
+              ),
+            ],
+          ),
           Expanded(
             child: Center(
-              child: Image.asset('assets/intermediate_image.png'), 
+              child: Image.asset('assets/kimtten.png'),
             ),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              ElevatedButton(
+              IconButton(
+                icon: Image.asset('assets/clothes.png'),
+                iconSize: 10,
                 onPressed: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => ScreenA()),
                   );
                 },
-                child: Text('Clothes'),
               ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ScreenB()),
-                  );
-                },
-                child: Text('Shop'),
-              ),
-              ElevatedButton(
+              IconButton(
+                icon: Image.asset('assets/toys.png'),
+                iconSize: 10,
                 onPressed: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => ScreenC()),
                   );
                 },
-                child: Text('Toys'),
               ),
-              ElevatedButton(
+              ],
+              ),
+             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  icon: Image.asset('assets/food.png'),
+                  iconSize: 10,
                 onPressed: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => Screen1()),
                   );
                 },
-                child: Text('Food Pantry'),
               ),
+                IconButton(
+                  icon: Image.asset('assets/shop.png'),
+                  iconSize: 10,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ScreenB()),
+                    );
+                  },
+                ),
             ],
           ),
           SizedBox(height: 16.0),
